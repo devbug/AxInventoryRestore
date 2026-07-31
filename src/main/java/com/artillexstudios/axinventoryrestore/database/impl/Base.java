90b12cfc664a2041caf55672e18565f8ecc51c57
BenceX100 <52270269+BenceX100@users.noreply.github.com>
2026-07-31 10:36:11 +0200

    3.15.1

--- src/main/java/com/artillexstudios/axinventoryrestore/database/impl/Base.java
+++ src/main/java/com/artillexstudios/axinventoryrestore/database/impl/Base.java
@@ -36,8 +36,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
--- src/main/java/com/artillexstudios/axinventoryrestore/guis/MainGui.java
+++ src/main/java/com/artillexstudios/axinventoryrestore/guis/MainGui.java
@@ -16,7 +16,6 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
